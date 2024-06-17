f4a6e48ca6a28a4a133a1c43d0522bd2d67d7beb
Marina Reis <marinareis.mr@gmail.com>
2024-06-17 17:10:57 -0300

    iniciando projeto

---
+++ src/test/java/com/marina/forum/hub/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marina.forum.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
